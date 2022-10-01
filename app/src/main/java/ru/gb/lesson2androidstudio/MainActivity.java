ee3dce455d64f5c5c039dd63bf67e41440128837
Angelina <angelinamscw@gmail.com>
2022-10-01 11:53:36 +0300

    Добавлены методы жизненного класса активити

--- app/src/main/java/ru/gb/lesson2androidstudio/MainActivity.java
+++ app/src/main/java/ru/gb/lesson2androidstudio/MainActivity.java
@@ -1,14 +1,53 @@
 package ru.gb.lesson2androidstudio;
 
+import android.os.Bundle;
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "happy MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate() called with: savedInstanceState = [" + savedInstanceState + "]");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.d(TAG, "onDestroy() called");
     }
 }
